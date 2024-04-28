c87167cba59a6b78c186b4f69dafbc12facd04cd
lookingforcharlie <charliefeng2020@gmail.com>
2024-04-27 21:41:56 -0400

    add a div

--- src/modal/Modal.tsx
+++ src/modal/Modal.tsx
@@ -37,20 +37,22 @@ export default function BootStrapModal(props: {
       style={{ display: 'block', position: 'initial' }}
     >
       <Modal.Dialog className='modal'>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
+        <div>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
 
-        <Modal.Body>
-          <p>Modal body text goes here.</p>
-        </Modal.Body>
+          <Modal.Body>
+            <p>Modal body text goes here.</p>
+          </Modal.Body>
 
-        <Modal.Footer>
-          <Button variant='secondary' onClick={handleOnClickOnButton}>
-            Close
-          </Button>
-          <Button variant='primary'>Save changes</Button>
-        </Modal.Footer>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={handleOnClickOnButton}>
+              Close
+            </Button>
+            <Button variant='primary'>Save changes</Button>
+          </Modal.Footer>
+        </div>
       </Modal.Dialog>
     </div>
   )
